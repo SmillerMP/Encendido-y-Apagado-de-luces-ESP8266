0ba7692958f8a2950e81c29a245304f2445664ca
SmillerMP <57497888+SmillerMP@users.noreply.github.com>
2021-04-15 12:29:15 -0600

    Update config.h

--- Wemos_Luz/config.h
+++ Wemos_Luz/config.h
@@ -1,14 +1,14 @@
 /************************ Configuracion IO Adafruit  *******************************/
 
 // Ingresar su cuenta y su IO KEY de IO Adafuit
-#define IO_USERNAME    "SamuelIMP"  // <-------- usuerio de cuenta IO Adafruit 
-#define IO_KEY         "aio_Bbqv34zChTNDe0PC0u7IlxkTmJTM"  // <-------- Clave o Token de cuenta IO Adafruit
+#define IO_USERNAME    "TU_USUARIO"  // <-------- usuario de cuenta IO Adafruit 
+#define IO_KEY         "TU_TOKE"  // <-------- Clave o Token de cuenta IO Adafruit
 
 /******************************* Configuration WIFI  **************************************/
 
 //Igresar nombre de su red WiFi y contraseña
-#define WIFI_SSID       "TURBONETT_4C70C4"  // <---- nombre de red
-#define WIFI_PASS       "10E8818E46" // <---- contraseña de red
+#define WIFI_SSID       "TU_RED"  // <---- nombre de red
+#define WIFI_PASS       "TU_CONTRASEÑA" // <---- contraseña de red
 
 #include "AdafruitIO_WiFi.h"
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
